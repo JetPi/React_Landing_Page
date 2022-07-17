bd9218d5146a782484f94bc1843b16074ccca9cf
Ibrahim Zarraga <ibrahimzarragaam@gmail.com>
2022-07-16 21:02:27 -0400

    Navbar done, named component menu

---
+++ src/js/component/menu.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Menu = () =>{
+    return(
+        <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+        <div className="container-fluid mx-2">
+            <a className="navbar-brand" href="#">Star Boostrap</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav position-absolute end-0 mx-2">
+                <a className="nav-link active" aria-current="page">Home</a>
+                <a className="nav-link">About</a>
+                <a className="nav-link">Services</a>
+                <a className="nav-link">Contact</a>
+            </div>
+            </div>
+        </div>
+        </nav>
+    );
+};
+
+export default Menu;
